9e4da20a01105ce6c983b729a53b13e306393ad0
Vinodkumar Bantu <v.bantu1@zensar.com>
2022-06-27 18:15:30 +0530

    codeexam05

---
+++ Codebased exams/University_Application/University_Application/Data/University_ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using University_Application.Models;
+
+namespace University_Application.Data
+{
+    public class University_ApplicationContext : DbContext
+    {
+        public University_ApplicationContext (DbContextOptions<University_ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<University_Application.Models.Student>? Student { get; set; }
+    }
+}
---
+++ Codebased exams/University_Application/University_Application/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using University_Application.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<University_ApplicationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("University_ApplicationContext") ?? throw new InvalidOperationException("Connection string 'University_ApplicationContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
